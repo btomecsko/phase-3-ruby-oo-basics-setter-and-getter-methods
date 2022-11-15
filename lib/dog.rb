78f1f599776ab243d569f088c17f22e48ed44f4e
Brandon T <98200133+btomecsko@users.noreply.github.com>
2022-11-15 15:08:27 -0600

    commit tree a97cf1b7812c00579586fbf0a5e59ca1bbb47887

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
 
+    #getter for name
+    def name
+        @name
+    end
+
+    #setter for name
+    def name=(name)
+        @name = name
+    end
+
+    #getter for breed
+    def breed
+        @breed
+    end
+
+    #setter for breed
+    def breed=(breed)
+        @breed = breed
+    end
+    
+
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
 
+     #getter for name
+     def name
+        @name
+    end
+     #setter for name
+     def name=(name)
+        @name = name
+    end
+     #getter for job
+     def job
+        @job
+    end
+     #setter for job
+     def job=(job)
+        @job = job
+    end
+
 end
